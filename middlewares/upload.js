c9bccf2bda1764f71315ac3423abf949c4a18005
aminmoj <aminmoj@gmail.com>
2023-04-22 18:28:26 -0400

    another try at fixing upload

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../public/images/");
+    cb(null, "images/");
   },
   filename: function (req, file, cb) {
     const name = Date.now() + "-" + file.originalname;
