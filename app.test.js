aa86c9081d5dcd370ebf4f74349726472c21afc2
aminmoj <aminmoj@gmail.com>
2023-05-25 18:31:00 -0400

    script build to echo 1

--- app.test.js
+++ app.test.js
@@ -2,32 +2,26 @@ const mongoose = require("mongoose");
 
 describe("Database Connection Test", () => {
   beforeAll(() => {
-    // Load environment variables
     require("dotenv").config();
 
-    // Connect to the MongoDB database
     mongoose.connect(
       "mongodb+srv://aminmoj:QvSPRlz7t50VVi9Q@cluster0.f4gxajs.mongodb.net/StarFleet_Comm_Module?retryWrites=true&w=majority"
     );
   });
 
   afterAll(async () => {
-    // Close the database connection
     await mongoose.connection.close();
   });
 
   it("should connect to the database", (done) => {
-    // Listen for the "connected" event
     mongoose.connection.once("connected", () => {
       done();
     });
   });
 
   //   it("should log an error if the database connection fails", (done) => {
-  //     // Simulate a connection error
   //     mongoose.connection.emit("error", new Error("Connection error"));
 
-  //     // Listen for the error event and verify the console log message
   //     console.log = jest.fn((message) => {
   //       expect(message).toContain("Connection error LCARS Down");
   //       done();
